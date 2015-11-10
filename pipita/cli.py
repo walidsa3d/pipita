fb4da14047fa6a53acab542d096d7d54c688cbbf
walid <walid.sa3d@gmail.com>
2015-11-10 19:01:07 +0100

    minimum working prototype

---
+++ pipita/cli.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import pipita
+
+print pipita.info('pipita')
--- pipita/pipita.py
+++ pipita/pipita.py
@@ -1,2 +1,24 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import requests
+
+
+class Pkg(object):
+    pass
+
+
+class Release(object):
+    pass
+
+
+def info(pkg_name):
+    pkg_url = 'https://pypi.python.org/pypi/{}/json'.format(pkg_name)
+    response = requests.get(pkg_url).json()
+    data = response['info']
+    pkg = Pkg()
+    pkg.author = data['author']
+    pkg.author_email = data['author_email']
+    pkg.license = data['license']
+    pkg.summary = data['summary']
+    pkg.latest_version = data['version']
+    return pkg
